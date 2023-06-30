2b1d7feb09ee52eeab806c9673e639a25f533d72
ax6030 <138182326+ax6030@users.noreply.github.com>
2023-06-30 18:07:42 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace TryCatchFinally
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstLine = "No first line was read";
+            try
+            {
+                var lines = File.ReadAllLines(args[0]);
+                firstLine = (lines.Length > 0) ? lines[0] : "The file was empty";
+            }
+            catch(Exception ex)
+            {
+                Console.Error.WriteLine("Cloud not read lines from the file : {0}", ex);
+            }
+            finally
+            {
+                Console.WriteLine(firstLine);
+            }
+        }
+    }
+}
